67af80272586278bd316389f3e2088e727fe67dc
Johannes Brandt Nielsen <johannes.brandt.nielsen@gmail.com>
2017-06-17 12:49:10 +0200

    add events association to account through users and plans

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,5 @@
 class Account < ApplicationRecord
   has_many :users, dependent: :destroy
   has_many :plans, through: :users
+  has_many :events, through: :plans
 end
--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -22,4 +22,10 @@ class AccountTest < ActiveSupport::TestCase
     assert_includes accounts(:jensen).plans, plans(:simons_plan)
     assert_includes accounts(:jensen).plans, plans(:simons_moms_plan)
   end
+
+  test '.events return events associated through plans' do
+    assert_includes accounts(:jensen).events, events(:simon_gets_up)
+    assert_includes accounts(:jensen).events, events(:simon_eats_breakfast)
+    refute_includes accounts(:jensen).events, events(:peter_gets_up)
+  end
 end
